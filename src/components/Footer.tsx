86984655ace4d28d0145fab4824b48bd41d45bc8
topdeveloper0908 <topdeveloper0908@gmail.com>
2023-04-22 18:56:36 +0200

    update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,7 +10,7 @@ const Footer: FunctionComponent<FooterProps> = ({
     <footer className="sticky-footer bg-white">
         <div className="container my-auto">
             <div className="copyright text-center my-auto">
-                <span>Copyright &copy; Your Website 2021</span>
+                <span>Copyright &copy; Your Website 2023</span>
             </div>
         </div>
     </footer>
