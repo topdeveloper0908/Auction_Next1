1b95fe9b6119377638e47541f3cbf864d17aeacc
topdeveloper0908 <topdeveloper0908@gmail.com>
2023-04-22 18:37:35 +0200

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Head from 'next/head'
+import '@fortawesome/fontawesome-svg-core/styles.css'
 import '@/styles/globals.css'
 import '@/styles/sb-admin-2.css'
-import '@/styles/dataTables.bootstrap4.min.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
